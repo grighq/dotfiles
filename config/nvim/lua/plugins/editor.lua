d60831bc94fdb9763fb4c14072d519c54141b2d4
grighq <grighq@gmail.com>
2026-05-31 21:47:14 +0300

    Delete fff config and add Snacks.

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -1,40 +1,95 @@
--- fff
-require("fff").setup({
-	prompt = "  ",
-	layout = {
-		show_scrollbar = false,
-		prompt_position = "top",
+local map = vim.keymap.set
+
+-- Snacks
+require("snacks").setup({
+	input = {
+		enabled = true,
 	},
-	keymaps = {
-		move_down = { "<tab>" },
-		move_up = { "<s-tab>" },
-		toggle_select = { "<c-j>" },
-		cycle_grep_modes = { "<c-f>" },
+	bigfile = {
+		enabled = true,
+	},
+	quickfile = {
+		enabled = true,
+	},
+	explorer = {
+		enabled = true,
+		trash = true,
+		replace_netrw = true,
+	},
+	picker = {
+		enabled = true,
+		win = {
+			input = {
+				keys = {
+					["<esc>"] = { "close", mode = { "n", "i" } },
+					["<tab>"] = { "list_down", mode = { "i", "n" } },
+					["<s-tab>"] = { "list_up", mode = { "i", "n" } },
+				},
+			},
+		},
 	},
 })
 
-vim.keymap.set("n", "sf", function()
-	require("fff").find_files()
-end, { desc = "Find files" })
+--- Keymaps
+map("n", "<leader>sb", function()
+	Snacks.picker.buffers()
+end, { desc = "Search buffers" })
 
-vim.keymap.set("n", "sc", function()
-	require("fff").find_files_in_dir(vim.fn.stdpath("config"))
-end, { desc = "Find config nvim" })
+map("n", "<leader>sf", function()
+	Snacks.picker.files()
+end, { desc = "Search files" })
 
-vim.keymap.set("n", "sg", function()
-	require("fff").live_grep()
-end, { desc = "Live grep" })
+map("n", "<leader>sc", function()
+	Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "Search config files" })
+
+map("n", "<leader>sg", function()
+	Snacks.picker.grep()
+end, { desc = "Grep" })
+
+map({ "n", "x" }, "<leader>sw", function()
+	Snacks.picker.grep_word()
+end, { desc = "Visual selection or word" })
+
+map("n", "<leader>sd", function()
+	Snacks.picker.diagnostics()
+end, { desc = "Diagnostics" })
+
+map("n", "<leader>s;", function()
+	Snacks.picker.commands()
+end, { desc = "Commands" })
+
+map("n", "<leader>e", function()
+	Snacks.explorer()
+end, { desc = "File explorer" })
+
+map("n", "<leader>bd", function()
+	Snacks.bufdelete()
+end, { desc = "Delete buffer" })
+
+map("n", "<leader>ba", function()
+	Snacks.bufdelete.all()
+end, { desc = "Delete all buffers" })
+
+map("n", "<leader>bi", function()
+	Snacks.bufdelete.invisible()
+end, { desc = "Delete all invisible buffers" })
+
+map("n", "<leader>bo", function()
+	Snacks.bufdelete.other()
+end, { desc = "Delete all buffers expect current one" })
 
 -- Neogit
 require("neogit").setup({})
 
-vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Open Neogit UI" })
+map("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Open Neogit UI" })
 
 -- Which-key
 require("which-key").setup({
 	preset = "helix",
 	spec = {
-		{ "<leader>s", group = "Telescope" },
+		{ "<leader>s", group = "Search" },
+		{ "<leader>d", group = "Buffer" },
 	},
 	win = {
 		title_pos = "center",
