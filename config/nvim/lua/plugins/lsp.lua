65afc8387dcc64f968ac5747bf215334eafe9f39
grighq <grighq@gmail.com>
2026-05-31 21:44:45 +0300

    Add Snacks to lua_ls diagnostics

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
 			runtime = { version = "LuaJIT" },
-			diagnostics = { globals = { "vim" } },
+			diagnostics = { globals = { "vim", "Snacks" } },
 			workspace = {
 				checkThirdParty = false,
 				library = {
