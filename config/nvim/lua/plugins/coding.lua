4c94d8a488b524497b964e645c45f6f57357ca71
grighq <grighq@gmail.com>
2026-06-06 21:06:19 +0300

    Delete <tab> next_char function in blink.cmp plugin

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -29,21 +29,7 @@ require("blink.cmp").setup({
 			end,
 			"fallback",
 		},
-		-- ["<tab>"] = { "select_next", "snippet_forward", "fallback" },
-		["<tab>"] = {
-			"select_next",
-			"snippet_forward",
-			function()
-				local line = vim.api.nvim_get_current_line()
-				local col = vim.api.nvim_win_get_cursor(0)[2]
-				local next_char = line:sub(col + 1, col + 1)
-				if next_char:match("[%)}%],]") then
-					vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], col + 1 })
-					return true
-				end
-			end,
-			"fallback",
-		},
+		["<tab>"] = { "select_next", "snippet_forward", "fallback" },
 		["<s-tab>"] = { "select_prev", "snippet_backward", "fallback" },
 	},
 })
